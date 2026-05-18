0967c20251d10570f777b1dd924ad02577115788
atmichalek <am2192@princeton.edu>
2026-05-18 08:34:35 -0400

    Add templates to setup tools

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="tiger_hlm_setup",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "tiger_hlm_setup": ["templates/**/*", "templates/*"],
+    },
     install_requires=[
         "numpy",
         "pandas",
